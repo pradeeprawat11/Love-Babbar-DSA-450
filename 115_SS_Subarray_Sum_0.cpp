4ab723def411a062c1b358d7a1bf02c3651e14ca
Pradeep Rawat <88647301+pradeeprawat11@users.noreply.github.com>
2023-05-16 22:02:54 +0530

    Add files via upload

---
+++ 115_SS_Subarray_Sum_0.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    //Function to count subarrays with sum equal to 0.
+    long long int findSubarray(vector<long long int> &arr, int n ) {
+        //code here
+        unordered_map<long long, int> map;
+        long long sum = 0;
+        long long ans = 0;
+        // innnitially insert 0 with frq 1
+        map[0] = 1;
+        
+        for(int i=0; i<n; i++) {
+            // calculate sum
+            sum += arr[i];
+            
+            if(map.find(sum) != map.end()) {
+                ans += map[sum];
+            }
+            // increse sum count in map
+            map[sum]++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n; //input size of array
+       
+        vector<long long int> arr(n,0);
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i]; //input array elements
+        Solution ob;
+        cout << ob.findSubarray(arr,n) << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+/*
+
+---- Approach ----
+1. Use map to count subarray with sum
+2. if sum repeats increate count of sum
+3. return count of zero in map
+
+*/
